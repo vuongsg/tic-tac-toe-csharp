097c7734e7b63cb73d949a33c9a91696d101c7ad
vuongsg <vuong.goteborg@gmail.com>
2021-10-03 02:41:59 +0700

    Full source

---
+++ src/TicTacToe/TicTacToe.ConsoleApp/GameHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace TicTacToe.ConsoleApp
+{
+	public class GameHelper
+	{
+		public int Minimax(Board board, int depth, bool isMax)
+		{
+			int score = board.Evaluate();
+
+			if (score == 10 || score == -10 || !board.IsMoveLeft())
+			{
+				return score;
+			}
+
+			//Try each node
+			if (isMax)
+			{
+				score = int.MinValue;
+
+				for (int i = 0, n = board.Size(); i < n; i++)
+					for (int k = 0; k < n; k++)
+					{
+						if (board.IsEmpty(i, k))
+						{
+							board.Set(i, k, "x");
+							score = Math.Max(score, Minimax(board, depth + 1, !isMax) - depth);
+							board.Set(i, k);    //clear
+						}
+					}
+			}
+			else
+			{
+				score = int.MaxValue;
+
+				for (int i = 0, n = board.Size(); i < n; i++)
+					for (int k = 0; k < n; k++)
+					{
+						if (board.IsEmpty(i, k))
+						{
+							board.Set(i, k, "o");
+							score = Math.Min(score, Minimax(board, depth + 1, !isMax) + depth);
+							board.Set(i, k);
+						}
+					}
+			}
+
+			return score;
+		}
+
+		/// <summary>
+		/// Computer tries to find best move. It always acts as maximizer
+		/// </summary>
+		/// <param name="board"></param>
+		/// <returns></returns>
+		public Tuple<int, int> FindBestMove(Board board)
+		{
+			int best = int.MinValue;
+			Tuple<int, int> points = new Tuple<int, int>(-1, -1);
+
+			for (int i = 0, n = board.Size(); i < n; i++)
+				for (int k = 0; k < n; k++)
+				{
+					if (board.IsEmpty(i, k))
+					{
+						board.Set(i, k, "x");
+						int score = Minimax(board, 1, false);
+
+						if (score > best)
+						{
+							best = score;
+							points = new Tuple<int, int>(i, k);
+						}
+
+						board.Set(i, k);
+					}
+				}
+
+			return points;
+		}
+	}
+}
---
+++ src/TicTacToe/TicTacToe.ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace TicTacToe.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("TIC TAC TOE Game");
+			Console.WriteLine("Computer takes x. You take o");
+			Board board = new Board();
+			GameHelper gameHelper = new GameHelper();
+			bool isMax = false;     //Human plays first
+			board.PrintBoard();
+
+			while (true)
+			{
+				if (!isMax)
+				{
+					Console.Write("Input your position that you want to make, e.g. you want to go in the very first cell, type 0,0");
+					string[] pos = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+					board.Set(int.Parse(pos[0]), int.Parse(pos[1]), "o");
+				}
+				else
+				{
+					Tuple<int, int> pos = gameHelper.FindBestMove(board);
+					board.Set(pos.Item1, pos.Item2, "x");
+				}
+
+				board.PrintBoard();
+				isMax = !isMax;
+				State state = board.CheckGameState(board);
+
+				if (state != State.Undefined)
+				{
+					switch (state)
+					{
+						case State.Computer:
+							Console.WriteLine("Computer won");
+							isMax = true;
+							break;
+						case State.Human:
+							Console.WriteLine("Congrats, you won");
+							isMax = false;
+							break;
+						case State.Draw:
+							Console.WriteLine("Draws");
+							isMax = false;
+							break;
+					}
+
+					Thread.Sleep(100);
+					board = new Board();
+					board.PrintBoard();
+				}
+			}
+		}
+	}
+}
