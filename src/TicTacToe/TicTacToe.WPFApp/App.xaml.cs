57039a27011424e14e3ad2cb34aefb6ed357d8ee
vuongsg <vuong.goteborg@gmail.com>
2022-02-10 15:59:55 +0700

    Add WPF project

---
+++ src/TicTacToe/TicTacToe.WPFApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TicTacToe.WPFApp;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+		MainWindow mainWnd = new MainWindow();
+		mainWnd.Show();
+	}
+}
---
+++ src/TicTacToe/TicTacToe.WPFApp/Converters/TurnToBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using TicTacToe.Helpers;
+
+namespace TicTacToe.WPFApp;
+
+internal class TurnToBooleanConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return (Turn)Enum.Parse(typeof(Turn), value.ToString()) == Turn.Human;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value == null || value == DependencyProperty.UnsetValue)
+		{
+			return Turn.Computer;
+		}
+
+		return (bool)value ? Turn.Human : Turn.Computer;
+	}
+}
